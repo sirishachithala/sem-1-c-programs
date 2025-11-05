c5c4e8eeb5b754e76aa5f8b1e482b5a8d0c66561
sirishachithala <sirishachithala1266@gmail.com>
2025-11-05 22:10:33 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	printf("sum of a and b:%d\n",a+b);
+	printf("difference of a and b:%d\n",a-b);
+	printf("product of a and b:%d\n",a*b);
+	printf("quotient of a and b:%d",a/b);
+	return 0;
+	
+}
