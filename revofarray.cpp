b9aa306cb571d82c29260acd10854d3b8395f6a4
sirishachithala <sirishachithala1266@gmail.com>
2025-11-06 10:15:58 +0530

    Add files via upload

---
+++ revofarray.cpp
@@ -0,0 +1,23 @@
+//REVERSE OF AN ARRAY.
+#include<stdio.h>
+int main()
+{
+	int i,n,a[10];
+	printf("enter n value:");
+	scanf("%d",&n);
+	printf("enter array elements:");
+	for (i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for (i=0;i<n;i++)
+	{
+	 printf("%d\n",a[i]);
+    }
+    printf("reverse of elements:");
+    for (i=n-1;i>=0;i--)
+    {
+    	printf("%d",a[i]);
+	}
+	return 0;
+}
