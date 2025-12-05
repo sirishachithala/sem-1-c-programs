f45bd499ea3a22f402f77f54ba7be4a82bba09a4
sirishachithala <sirishachithala1266@gmail.com>
2025-12-05 10:27:39 +0530

    Add files via upload

---
+++ pointerdec--.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	int *p=&a;
+	printf("address of a:%p\n",&a);
+	printf("address by using pointer:%p\n",p);
+	p--;
+	printf("after decreament address is :%p\n",p);
+}
---
+++ pointers1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=15;
+	int *p;
+	p=&a;
+	printf("%d\n",a);
+	printf("%d\n",&a);
+	printf("%p\n",p);
+	printf("%d\n",*p);
+}
